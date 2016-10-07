8f25c450de0bfc53ba653ad98e70129f32206fae
bion <bionjohnson@gmail.com>
2016-10-06 22:48:09 -0700

    Convert attachments to pdf on write

--- lib/attachment.rb
+++ lib/attachment.rb
@@ -21,4 +21,16 @@ class Attachment
 
     File.open(path, "wb") { |f| f.write(data.read) }
   end
+
+  def write_as_pdf_to(final_path)
+    return write_to(final_path) if File.extname(filename) == ".pdf"
+
+    original_tmp_path = "tmp/#{filename}"
+    pdf_tmp_path = "#{File.basename(filename, '.*')}.pdf"
+
+    write_to(tmp_path)
+    `soffice --convert-to pdf #{original_tmp_path} --headless`
+
+    FileUtils.mv(pdf_tmp_path, final_path)
+  end
 end
